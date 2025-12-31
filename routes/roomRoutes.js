4a7254c93ef82bf69e3c8aa9c2759df1da28dc46
Mohamed Sheheto <113710619+mohamed-sheheto@users.noreply.github.com.>
2025-12-31 23:58:30 +0200

    redesign frontend

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -5,12 +5,15 @@ const roomController = require("../controllers/roomController");
 const router = express.Router();
 
 router.use(authController.protect);
+
 router.route("/").get(roomController.getRooms).post(roomController.createRoom);
+
 router
   .route("/:id")
-  .post(roomController.leaveRoom)
-  .delete(roomController.deleteRoom);
+  .delete(roomController.deleteRoom)
+  .get(roomController.getRoom);
 
 router.post("/:id/join", roomController.joinRoom);
 router.post("/:id/leave", roomController.leaveRoom);
+
 module.exports = router;
